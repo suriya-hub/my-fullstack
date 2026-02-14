6c6a70bea0d57e657524826ec6055c08fd1573e8
suriyaB <suriyasmart619@gmail.com>
2026-02-14 12:10:12 +0530

    image api init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState("")
+  const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_ACCESS;
+
+
+  const handleSubmit = () => {
+    console.log(search, 'search')
+    fetch(`https://api.unsplash.com/photos/random?query=${search}&client_id=${ACCESS_KEY}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err))
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="search" placeholder="Enter your name" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <button onClick={() => handleSubmit()}>search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
